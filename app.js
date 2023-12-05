276f676246bb08008d03b3fbebbaabaefb29a79b
SpicyAndWolf <heluoshiyongyuandishen@gmail.com>
2023-12-05 16:57:24 +0800

    2023-12-05

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const userRouter = require('./router/user')
+const userInfoRouter = require('./router/userInfo')
 
 //创建服务器
 const app = express()
@@ -24,6 +25,13 @@ app.use((req, res, next) => {
 
 //导入路由模块
 app.use('/api', userRouter)
+app.use('/my', userInfoRouter)
+
+//错误级别中间件
+app.use((err, req, res, next) => {
+    //默认错误处理
+    res.cc(err)
+})
 
 //启动服务器
 app.listen(80, () => {
---
+++ router/userInfo.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const config = require('../config')
+const userInfo_Handler = require('../router_handler/userInfo')
+
+//挂载路由函数
+router.get('/userInfo', userInfo_Handler.getUserInfo)
+
+//暴露函数
+module.exports = router
